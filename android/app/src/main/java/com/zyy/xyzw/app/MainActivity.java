6cb4fa78b75b1e90dcdbaf4259664914344ab6a2
tybl91314-star <tybl91314@gmail.com>
2026-03-09 19:51:08 +0800

    提交吧

---
+++ android/app/src/main/java/com/zyy/xyzw/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zyy.xyzw.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zyy.xyzw.app',
+  appName: '天涯咸鱼',
+  webDir: 'dist'
+};
+
+export default config;
